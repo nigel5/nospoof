920149aa4311a718394a1378a80880414ca34920
Nigel Huang <28766663+nigel5@users.noreply.github.com>
2019-03-11 18:33:30 -0400

    Use routing to separate driver and receiver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { JobsComponent } from './components/jobs/jobs.component';
+import { RecieverComponent } from './components/reciever/reciever.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'driver', component: JobsComponent },
+  { path: 'reciever', component: RecieverComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { JobsComponent } from './components/jobs/jobs.component';
-import { NospoofService } from './services/nospoof.service';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,35 +6,5 @@ import { NospoofService } from './services/nospoof.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'nospoof';
-  profile: any;
-  currentJob: any;
-  constructor(private ns: NospoofService) { }
-
-  ngOnInit() {
-    var truckId = (<HTMLInputElement>document.getElementById('jobId')).value
-    this.ns.getProfile("NakLrvNzjDx5TDcbzWjM").subscribe(
-      next => {
-        // Have profile, now get current job...
-        this.ns.getJobById(next["data"]["truck"]["currentJob"]).subscribe(
-          job => {
-            this.currentJob = job;
-          }
-        )
-        this.profile = next
-      }, err => {
-        console.log(err)
-      }, () => {
-        console.log("Done!")
-      }
-    )
-  }
-
-  get originLocation() {
-    return this.currentJob["data"]["job"]["origin"]
-  }
-
-  get destinationLocation() {
-      return this.currentJob["data"]["job"]["destination"]
-  }
+  title = 'NoSpoof - Demo app';
 }
